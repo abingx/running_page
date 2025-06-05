4d70304e244310b9b56aa8a3124f998758932f6a
Sit <allinschroe@gmail.com>
2025-06-05 09:46:05 +0800

    web fix

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -54,7 +54,8 @@ const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength: number, year: string): string => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
-  return `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据`;
+  //`我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据`
+  return `当身体到达极限时，意志会带你杀出重围`; 
 };
 const ENGLISH_INFO_MESSAGE = (yearLength: number, year: string): string =>
   `Running Journey with ${yearLength} Years, the table shows year ${year} data`;
