0f57559cf5500424430be2b4f57b874925c0ee00
Sit <allinschroe@gmail.com>
2025-10-21 10:31:16 +0800

    重新开始

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,17 +16,13 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://running.allinhub.dpdns.org',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Summary',
       url: `${getBasePath()}/summary`,
-    },
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
     },
     {
       name: 'About',
